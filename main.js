500b3b97a85f5efd3a7530e165d6b08c58d6d995
kevinngonzales <kevinngstem@gmail.com>
2023-12-16 00:06:14 -0800

    updated notes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./dataHandling";
 import { displayQuestionsAndAnswers } from "./renderHandling";
 import { audioFeatures } from "./audio";
-
+// import functions from other modules
 
 //use async with try - catch to fetch and display data
 async function fetchDataAndDisplay() {
@@ -11,6 +11,7 @@ async function fetchDataAndDisplay() {
         //use function from other .js file and pass api's data through it
         displayQuestionsAndAnswers(data);
         console.log('Success');
+        //catch keyword to catch error and console.log it
     } catch (error) {
         console.error(error);
     }
@@ -19,10 +20,13 @@ async function fetchDataAndDisplay() {
 // call the function
 fetchDataAndDisplay();
 
+// grab button from html to store in js variable 
 let reloadWebpageButton = document.getElementById('reloadWebpageButton');
+// add event listener to js variable so that if clicked, the we page is reloaded and then we get a new random set of questions
 reloadWebpageButton.addEventListener('click', () => {
     location.reload();
 })
 
+//call this function to set volume of background music
 audioFeatures();
 
--- renderHandling.js
+++ renderHandling.js
@@ -1,29 +1,37 @@
+//export function to be used in main.js
 export { displayQuestionsAndAnswers };
 
+// grab sound effects from html and store it in js variables
 const rightSoundEffect = document.getElementById('rightSoundEffect');
-
 const wrongSoundEffect = document.getElementById('wrongSoundEffect');
 
 function displayQuestionsAndAnswers(question) {
-    //grab html and declare it as js variable
+    //grab html and declare it as js variable to be used to display questions/answers
     const questionContainer = document.getElementById('questionContainer');
     //initialize the element as empty
     questionContainer.innerHTML = '';
 
+
+    //use for loop to iterate through api's data
     for (let i = 0; i < question.length; i++) {
         if (question[i].type === 'multiple') {
             //displaying questions
+            //create p element and store it in variable
             const questions = document.createElement('p');
+            //set text content as index + 1 for question number and the question itself at that specific index
             questions.innerHTML = `${i + 1}. ${question[i].question}`;
+            //append the questions to the question container declared earlier
             questionContainer.appendChild(questions);
 
             //create new array of combined right answer and wrong answers
             //decode correct answer so it matches the html text
+            // used [i] to store data from that specific index
             let correctAnswer = decodeNumericCharacter(question[i].correct_answer);
             let wrongAnswers = question[i].incorrect_answers;
+            //use spread operator to combine arrays
             let allAnswers = [correctAnswer, ...wrongAnswers];
 
-            //randomly shuffle answers 
+            //randomly shuffle answers using math.random() to generate random number between -0.5 - 0.5 and then using .sort() to randomly sort the array so that the right answer is not always the first option
             function shuffleAnswers(array) {
                 return array.sort(() => Math.random() - 0.5);
             }
@@ -43,14 +51,19 @@ function displayQuestionsAndAnswers(question) {
             })
 
             //create buttons for answers
+            //use for loop to iterate through the answers we decoded/randomized
             for (let j = 0; j < allAnswers.length; j++) {
+                //create button elements and store in answer button variable
                 const answerButton = document.createElement('button');
+                // set text content of answerbutton to the answer at the specific index we're iterating through
                 answerButton.innerHTML = `${allAnswers[j]}`
+                // append button with answers as text content to the question container
                 questionContainer.appendChild(answerButton);
 
                 //if correct/wrong answer is chosen change colors accordingly
                 //also play audio 
                 if (answerButton.innerHTML === correctAnswer) {
+                    //use event listener to "listen" for click and then play sound effect and change background color to green
                     answerButton.addEventListener('click', () => {
                         rightSoundEffect.play();
                         answerButton.style.backgroundColor = "#1bfc06";
@@ -58,39 +71,46 @@ function displayQuestionsAndAnswers(question) {
 
                 } else {
                     answerButton.addEventListener('click', () => {
+                        //use event listener to "listen" for click and then play sound effect and change background color to red
                         wrongSoundEffect.play();
                         answerButton.style.backgroundColor = "red";
                     })
                 }
             }
         }
-
+        // use else if statement to determine if it is a true or false question
         else if (question[i].type === 'boolean') {
+            // create paragraph element and store it in questions variable
             const questions = document.createElement('p');
+            // set text content of element to index number + 1 and the question
             questions.innerHTML = `${i + 1}. ${question[i].question}`;
+            // append the question to container
             questionContainer.appendChild(questions);
 
             //displaying true/false buttons
             const trueButton = document.createElement('button');
             const falseButton = document.createElement('button');
-
+            //set text to buttons to true/false
             trueButton.innerHTML = 'True';
             falseButton.innerHTML = 'False';
-
+            //append buttons to container
             questionContainer.appendChild(trueButton);
             questionContainer.appendChild(falseButton);
 
             //if correct button is clicked, change color accordingly
             trueButton.addEventListener('click', () => {
+                // if button that is clicked matches correct answer then play right sound effect and change color to green
                 if (trueButton.innerHTML === question[i].correct_answer) {
                     rightSoundEffect.play();
                     trueButton.style.backgroundColor = "#1bfc06";
+                    // if not then play wrong sound effect and change color to red
                 } else {
                     wrongSoundEffect.play();
                     trueButton.style.backgroundColor = "red";
                 }
             })
             falseButton.addEventListener('click', () => {
+                // same thing here, just checking if false is the correct answer
                 if (falseButton.innerHTML === question[i].correct_answer) {
                     rightSoundEffect.play();
                     falseButton.style.backgroundColor = "#1bfc06";
